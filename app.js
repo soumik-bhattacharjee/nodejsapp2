a282119232935bfe267fe4e12186059febb884a8
soumik bhattacharjee <soumik_bhattacharjee@hotmail.com>
2020-07-03 20:09:21 +0530

    commit

---
+++ app.js
@@ -0,0 +1,7 @@
+//TODO :  A web application
+var express = require('express');
+var app = express();app.get('/', function (req, res) {
+  res.send('Hello World!');
+});app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
